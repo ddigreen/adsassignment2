95160030da6f45f44f1490ea3e361c65ede7dfd6
amangeldiyevdiasbek <diasbek2005@gmail.com>
2025-04-13 22:59:02 +0500

    my list interface

---
+++ src/MyLinkedList.java
@@ -0,0 +1,21 @@
+public class MyLinkedList<T> implements MyList<T> {
+
+    private class MyNode {
+        T data;
+        MyNode next;
+        MyNode prev;
+
+        MyNode(T data) {
+            this.data = data;
+        }
+    }
+
+    private MyNode head;
+    private MyNode tail;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
