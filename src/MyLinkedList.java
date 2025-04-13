807c42a70188a7de2c5c2d287cc50a752850f0d0
amangeldiyevdiasbek <diasbek2005@gmail.com>
2025-04-13 23:59:06 +0500

    my linked list

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -35,6 +35,35 @@ public class MyLinkedList<T> implements MyList<T> {
     }
 
     @Override
+    public void add(int index, T element) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        if (index == size) {
+            add(element);
+            return;
+        }
+        MyNode newNode = new MyNode(element);
+        if (index == 0) { // Insert at head.
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        } else {
+            MyNode current = head;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+            MyNode prevNode = current.prev;
+            prevNode.next = newNode;
+            newNode.prev = prevNode;
+            newNode.next = current;
+            current.prev = newNode;
+        }
+        size++;
+    }
+
+
+    @Override
     public T get(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
@@ -47,6 +76,39 @@ public class MyLinkedList<T> implements MyList<T> {
     }
 
     @Override
+    public T remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        MyNode toRemove;
+        if (index == 0) { // Remove head.
+            toRemove = head;
+            head = head.next;
+            if (head != null) {
+                head.prev = null;
+            } else { // List becomes empty.
+                tail = null;
+            }
+        } else if (index == size - 1) { // Remove tail.
+            toRemove = tail;
+            tail = tail.prev;
+            tail.next = null;
+        } else {
+            toRemove = head;
+            for (int i = 0; i < index; i++) {
+                toRemove = toRemove.next;
+            }
+            MyNode prevNode = toRemove.prev;
+            MyNode nextNode = toRemove.next;
+            prevNode.next = nextNode;
+            nextNode.prev = prevNode;
+        }
+        size--;
+        return toRemove.data;
+    }
+
+
+    @Override
     public int size() {
         return size;
     }
@@ -56,6 +118,25 @@ public class MyLinkedList<T> implements MyList<T> {
         head = null;
         tail = null;
         size = 0;
+    }
+
+    @Override
+    public java.util.Iterator<T> iterator() {
+        return new java.util.Iterator<T>() {
+            private MyNode current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public T next() {
+                T data = current.data;
+                current = current.next;
+                return data;
+            }
+        };
     }
 
     @Override
