cfe4f243a650a3f55ea080f408638b4eb18aa7e0
amangeldiyevdiasbek <diasbek2005@gmail.com>
2025-04-13 23:00:40 +0500

    my list interface

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -19,3 +19,59 @@ public class MyLinkedList<T> implements MyList<T> {
         tail = null;
         size = 0;
     }
+
+    @Override
+    public void add(T element) {
+        MyNode newNode = new MyNode(element);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        MyNode current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        MyNode current = head;
+        while (current != null) {
+            sb.append(current.data);
+            if (current.next != null) {
+                sb.append(", ");
+            }
+            current = current.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+}
